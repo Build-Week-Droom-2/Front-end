3101b05e21d17becfdcae3237207095f9098a27b
Enkhbilguun Nomch <bbg.1015@gmail.com>
2019-08-27 15:36:36 -0500

    merged and fixed conflicts

--- droom/src/App.js
+++ droom/src/App.js
@@ -1,10 +1,14 @@
 import React, {useState, createContext} from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Login from './components/Login/Login'
 import PrivateRoute from './components/Login/PrivateRoute'
+
+//imported components
+import SplashPage from './components/SplashPage/SplashPage'
+import Login from './components/Login/Login'
 import UserPage from './components/UserPage/UserPage';
 import JobPage from './components/JobProfile/JobPage';
 import UserRegistrationSplash from './components/Registration/UserRegistrationSplash';
+import GeneralRegistration from './components/Registration/GeneralRegistration'
 import WelcomeBack from './components/Login/WelcomeBack';
 
 import './App.css';
@@ -20,11 +24,11 @@ function App() {
         
         {/* <UserRegistration /> */}
         {/* <CompanyRegistration /> */}
-        {/* <SplashPage /> */}
-        <Link to='/login'>Login</Link>
+        <SplashPage />
+        {/* <Link to='/login'>Login</Link> */}
         <Route path='/login' component={Login}/>
         {/* <PrivateRoute exact path='/' component={} /> */}
-        <Route path='/register' component={UserRegistration} />
+        <Route path='/register' component={WelcomeBack} />
        </Router>
     </DroomData.Provider>
   );
