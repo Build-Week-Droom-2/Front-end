d5a6751337c62f7ca6714b014577553b5797589b
Krishan Nattar <krishan.nattar@gmail.com>
2019-08-28 08:06:30 -0700

    refactor

--- droom/src/App.js
+++ droom/src/App.js
@@ -6,15 +6,6 @@ import PrivateRoute from './components/Login/PrivateRoute'
 import SplashPage from './components/SplashPage/SplashPage'
 import Login from './components/Login/Login'
 import WelcomeBack from './components/Login/WelcomeBack';
-// import UserPage from './components/UserPage/UserPage';
-// import JobPage from './components/JobProfile/JobPage';
-// import UserRegistrationSplash from './components/Registration/UserRegistrationSplash';
-// import GeneralRegistration from './components/Registration/GeneralRegistration';
-// import JobListings from './components/JobProfile/JobListings';
-// import EmployeeMatch from './components/Matches/EmployeeMatch';
-// import JobMatch from './components/Matches/JobMatch';
-// import UserRegistration from './components/Registration/UserRegistration';
-// import RecruiterRegistration from './components/Registration/RecruiterRegistration';
 
 import './App.css';
 
